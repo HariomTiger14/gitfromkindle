f875dab3f3d7d6b6e1b1cea1682d80b29c1eb60c
Hari Om Bhatia <hariomritesh@gmail.com>
2023-05-21 23:10:18 +0530

    push code from feature branch to origin branch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,5 @@ public class App
         System.out.println("code line 2 added on git hub website");
         System.out.println("code line added not knowing that code line 2 added on git hub website");
         System.out.println("local code line added to overwrite a line added on git hub repo");
-        System.out.println("code line on practice branch");
     }
 }
